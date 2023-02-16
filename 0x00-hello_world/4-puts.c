c466d42ec032654f1cea18febd0af41a8641a5a1
marycathlin <marycathln1@gmail.com>
2023-02-16 08:22:33 -0800

    Hello,puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #!/bin/bash
 #include <stdio.h>
 
-
 /**
- * main - entry point
- * 
- * Return: Always 0 (success)
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	return (0)
+    return (0);
 }
-
