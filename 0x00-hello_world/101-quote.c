0427210876426e2f7442b6c830457e7275afe96f
marycathlin <marycathln1@gmail.com>
2023-02-16 13:12:45 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,14 +3,15 @@
 /**
  * main - Entry Point
  *
- * Return: Always 1
+ * Description: prints and that piece of art is useful" - Dora Korpar, 2015-10-19
+ *
+ * Return: Always 1(Error)
  */
-
 int main(void)
 {
-    write(2,
-         "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-          59);
-return (1);
+       write(2,
+	    "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+     return (1);
 
 }
