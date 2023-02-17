9b41a0a74f52a308e17cca4312afb0793e1efcdd
marycathlin <marycathln1@gmail.com>
2023-02-17 14:05:16 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,21 +11,21 @@
 int main(void)
 
 {
-	int d, p, q;
+	int digit1, digit2, digit3;
 
-	for (d = '0'; d < '9'; d++)
+	for (digit1 = '0'; digit1 < '8'; digit1++)
 	{
-		for (p = d + 1; p <= '9'; p++)
+		for (digit2 = digit1 + 1; digit2 <= '9'; digit2++)
 		{
-			for (q = p + 1; q <= '9'; q++)
+			for (digit3 = digit2 + 1; digit3 <= '10'; digit3++)
 
 			{
-				if ((p != d) != q)
+				if ((digit2 != digit1) != digit3)
 				{
-					putchar(d);
-					putchar(p);
-					putchar(q);
-					if (d == '7' && p == '8')
+					putchar((digit1 % 10) + '0');
+					putchar((digit2 % 10) + '0');
+					putchar((digit3 % 10) + '0');
+					if (digit1 == 7 && digit2 == 8 digit3 == 9)
 						continue;
 					putchar(',');
 					putchar(',');
