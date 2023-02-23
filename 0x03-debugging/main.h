f03daeb45fae78e0cfa1c73ac0d3f666caf44be0
marycathlin <marycathln1@gmail.com>
2023-02-23 04:52:41 -0800

    0-main.c, main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,4 @@
 #ifndef MAIN_H
-
 #define MAIN_H
 
 #include <stdio.h>
@@ -10,3 +9,4 @@ int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
+
