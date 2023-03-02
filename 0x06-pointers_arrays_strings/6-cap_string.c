8458dcae3585656ed421857520b61a65d6ac9dcc
marycathlin <marycathln1@gmail.com>
2023-03-02 03:38:26 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,40 +1,50 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
- *
- * Return: A pointer to the changed string.
- */
-
-char *cap_string(char *str)
+* is_sep - check if a char is a seperator.
+* @s: the char to test.
+*
+* Return: 1 if seperator is found else 0
+*/
+int is_sep(char s)
 {
-	int index = 0;
+	char sep[] = {'\t', '\n', ' ', ',', ';', '!',
+		      '.', '?', '\"', '(', ')', '{', '}'};
+	int i = 0;
 
-	while (str[index])
+	while (sep[i] != '\0')
 	{
-		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			index++;
-
-		if (str[index - 1] == ' ' ||
-		    str[index - 1] == '\t' ||
-		    str[index - 1] == '\n' ||
-		    str[index - 1] == ',' ||
-		    str[index - 1] == ';' ||
-		    str[index - 1] == '.' ||
-		    str[index - 1] == '!' ||
-		    str[index - 1] == '?' ||
-		    str[index - 1] == '"' ||
-		    str[index - 1] == '(' ||
-		    str[index - 1] == ')' ||
-		    str[index - 1] == '{' ||
-		    str[index - 1] == '}' ||
-		    index == 0)
-			str[index] -= 32;
-
-		index++;
+		if (s == sep[i])
+			return (1);
+		i++;
 	}
-
-	return (str);
+	return (0);
 }
+
+/**
+* cap_string - This function capitalizes all words of a string.
+* @s: sring to be processed.
+*
+* Return: pointer to the modified string.
+*/
+char *cap_string(char *s)
+{
+	int sep, i;
+
+	sep = 1;
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (sep == 1 && (s[i] >= 'a' && s[i] <= 'z'))
+		{
+			s[i] -= 32;
+			sep = 0;
+		}
+		sep = is_sep(s[i]);
+		i++;
+}
+	return (s);
+}
+
+
 
