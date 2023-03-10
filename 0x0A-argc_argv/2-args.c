1f3b49cc8d65e55dd3e9b1660bf52de40073c6ca
marycathlin <marycathln1@gmail.com>
2023-03-10 08:33:53 -0800

    i can do anything

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; ++i)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
