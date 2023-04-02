e64e90e29a5f74614c0e75b2c2f141990fc0cbcf
marycathlin <marycathln1@gmail.com>
2023-04-02 08:47:06 -0700

    singly linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,21 +12,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nodes = 0;
+	size_t node = 0;
 
 	while (h != NULL)
 	{
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
 		else
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
-		nodes++;
-
 		h = h->next;
+		node++;
 	}
-	return (nodes);
+	return (node);
 }
+
